1ea3879ea60f08b7f6a5f406cf61f00f312cd5fc
arminkoeck <119230167+arminkoeck@users.noreply.github.com>
2024-05-10 09:56:55 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+(self["webpackChunkproject_template"] = self["webpackChunkproject_template"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("hi");
+console.log("hola");
+console.log("hey");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Byb2plY3QtdGVtcGxhdGUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJoaVwiKTtcbmNvbnNvbGUubG9nKFwiaG9sYVwiKTtcbmNvbnNvbGUubG9nKFwiaGV5XCIpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
